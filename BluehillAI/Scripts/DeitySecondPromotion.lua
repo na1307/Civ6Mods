6bd2da789f6317dd87827e63889c056e9ae27d43
Jonghyo Lee <na1307@outlook.kr>
2024-08-24 19:40:53 +0900

    AI

---
+++ BluehillAI/Scripts/DeitySecondPromotion.lua
@@ -0,0 +1,23 @@
+﻿function IsCombatUnit(unit)
+	local unitInfo = GameInfo.Units[unit:GetType()]
+
+	-- Check if the unit has combat strength or ranged combat strength
+	if unitInfo.Combat > 0 or unitInfo.RangedCombat > 0 then
+		return true
+	else
+		return false
+	end
+end
+
+function OnUnitPromoted( playerID, unitID )
+	local playerConfig = PlayerConfigurations[playerID]
+	local difficultyInfo = GameInfo.Difficulties[playerConfig:GetHandicapTypeID()]
+	local unit = UnitManager.GetUnit(playerID, unitID)
+	local unitExperience = unit:GetExperience()
+
+	if IsCombatUnit(unit) and (difficultyInfo.DifficultyType == 'DIFFICULTY_DEITY') and not PlayerManager.GetPlayer(playerID):IsHuman() and (unitExperience:GetExperiencePoints() <= 15) then
+		unitExperience:ChangeExperience(30)
+	end
+end
+
+Events.UnitPromoted.Add(OnUnitPromoted)
